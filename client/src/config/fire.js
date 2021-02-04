efe1bef3abce2863c5b871183cc02f616c896a83
C.J <cj@kingcountry.ca>
2021-02-04 12:47:22 -0800

    update package.json

--- client/src/config/fire.js
+++ client/src/config/fire.js
@@ -13,3 +13,20 @@ const config = {
 
   const fire = firebase.initializeApp(config);
   export default fire;
+
+    // proces.env.REACT_APP_FIREBASE_API_KEY;
+    // proces.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+    // proces.env.REACT_APP_FIREBASE_PROJECT_ID;
+    // proces.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+    // proces.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+    // proces.env.REACT_APP_FIREBASE_APP_ID;
+    // proces.env.REACT_APP_FIREBASE_DATABASE_URL;
+
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    // measurementId:  process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
